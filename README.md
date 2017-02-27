970eb62954dcc9ecca173f4fdfeef285b5c49ee9
Scott Banwart <sbanwart@rogue-technology.com>
2017-02-26 21:56:24 -0500

    Add README file. Complete empty setup.py file.

---
+++ README.md
@@ -0,0 +1,13 @@
+# Generator Morepath
+--------------------
+
+Yeoman generator for the Morepath Python microframework.
+
+## Usage
+
+* Create a directory for your project
+    * >mkdir my-project && cd my-project
+* Run the generator to create a skeleton Morepath module
+    * >yo Morepath
+* Enter the project name at the prompt
+* Start building your web service
--- generators/app/templates/setup.py
+++ generators/app/templates/setup.py
@@ -1,20 +1,34 @@
+"""A setuptools based setup module.
+See:
+https://packaging.python.org/en/latest/distributing.html
+
+This is an empty setup.py for this module. The project name will be
+pre-populated by the generator.
+"""
+
 from setuptools import setup, find_packages
 
-name = '<%= project_name %>'
-description = ('')
-version = '0.1.0'
-
 setup(
-    name=name,
-    version=version,
-    description=description,
+    name='<%= project_name %>',
+    version='0.1.0',
+    description='',
+    long_description='',
+    url='',
+    author='',
+    author_email='',
+    license='',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 3',
+    ],
+    keywords='',
     packages=find_packages(),
     install_requires=[
         'morepath>=0.14',
     ],
-    entry_points=dict(
-        console_scripts=[
-            'run-app = <%= project_name %>.__main__:run',
+    entry_points={
+        'console_scripts': [
+            'run-app=<%= project_name %>.__main__:run',
         ],
-    )
+    },
 )
